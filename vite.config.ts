396bc0c283353614d86ad0f9dbad40e28bde770c
Ayomide Akinola <drayfocus@gmail.com>
2025-04-23 16:56:06 +0100

    fixing: h-screen issie

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/UserMobileApp/",
+  base: "/UserMobileApp/",
   plugins: [vue(), legacy(), tailwindcss()],
   resolve: {
     alias: {
